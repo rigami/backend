5289a4fa19a8da440d8c7fa3f65a2c53cf648d4c
Danilkinkin <danilkinkin@gmail.com>
2021-09-24 20:44:23 +0400

    [main] Disable `X-Powered-By` header

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(
         new ValidationPipe({
             // TODO: Use in production disableErrorMessages: true,
         }),
     );
+    app.disable('x-powered-by');
     await app.listen(8080);
 }
 bootstrap();
