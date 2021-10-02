82863bc645f615934861104093b1b65608fbea37
Danilkinkin <danilkinkin@gmail.com>
2021-10-03 02:07:43 +0400

    [generateMergeCode] Remove similar symbols

--- src/users/utils/generateMergeCode.ts
+++ src/users/utils/generateMergeCode.ts
@@ -1,6 +1,6 @@
 import { sample } from 'lodash';
 
-const alphabet = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
+const alphabet = 'QWERTYUPASDFGHJKLZXCVBNM23456789';
 
 function generateMergeCode(length = 4): string {
     return Array.from({ length })
